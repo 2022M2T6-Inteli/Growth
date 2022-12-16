177dcc596cd2c65dfe06f39c317a295fdb08e133
SamuelLucas-007 <samuel.almeida@sou.inteli.edu.br>
2022-12-15 21:09:58 -0300

    Ajuste rotas de update

--- src/Backend/routes/Web/UpdateUserRoutes.js
+++ src/Backend/routes/Web/UpdateUserRoutes.js
@@ -7,12 +7,12 @@ const AuthMiddleware = require('../../middlewares/AuthMiddleware')
 const Router = express.Router()
 
 Router.get("/contador", AuthMiddleware.onlyLoggedSite, Controller.getEdicaoUsuarioContador)
-Router.post("/contador", AuthMiddleware.onlyLoggedSite, Controller.getEdicaoUsuarioContador)
+Router.post("/contador", AuthMiddleware.onlyLoggedSite, Controller.postEdicaoUsuarioContador)
 
 Router.get("/empresa", AuthMiddleware.onlyLoggedSite, Controller.getEdicaoUsuarioEmpresa)
-Router.post("/empresa", AuthMiddleware.onlyLoggedSite, Controller.getEdicaoUsuarioEmpresa)
+Router.post("/empresa", AuthMiddleware.onlyLoggedSite, Controller.postEdicaoUsuarioEmpresa)
 
-Router.get("/dono", AuthMiddleware.onlyLoggedSite, Controller.getEdicaoUsuarioDono)
-Router.get("/dono", AuthMiddleware.onlyLoggedSite, Controller.getEdicaoUsuarioDono)
+Router.get("/responsavel", AuthMiddleware.onlyLoggedSite, Controller.getEdicaoUsuarioDono)
+Router.get("/responsavel", AuthMiddleware.onlyLoggedSite, Controller.postEdicaoUsuarioDono)
 
 module.exports = Router;
